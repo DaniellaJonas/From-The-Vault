ec90c75bb10cd329f23df77e0e48788fcb98f9f9
DaniellaJonas <daniella.jonas11@gmail.com>
2024-06-01 19:53:28 +0300

    jangri

--- Indian/Sweet/sweetIn.js
+++ Indian/Sweet/sweetIn.js
@@ -1,3 +1,11 @@
 function SH(){
     window.location.href = "./Sooji Halwa/sh.html"
 }
+
+function Karadantu(){
+    window.location.href = "./Karadantu/karadantu.html"
+}
+
+function Jangri(){
+    window.location.href = "./Jangri/jangri.html"
+}
