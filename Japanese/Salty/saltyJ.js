6d5a61c159efab7e50df7ac3ce0ac863b104b40a
Yaaritazran09 <yaarazran09@gmail.com>
2024-06-05 18:20:31 +0300

    new

---
+++ Japanese/Salty/saltyJ.js
@@ -0,0 +1,12 @@
+function Ramen(){
+    window.location.href = "./Ramen/ramen.html"
+}
+
+function Okonomiyaki(){
+    window.location.href = "./Okonomiyaki/okonomiyaki.html"
+}
+
+
+function Tempura(){
+    window.location.href = "./Tempura/tempura.html"
+}
