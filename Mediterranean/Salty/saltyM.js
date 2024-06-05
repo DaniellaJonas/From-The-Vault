22a5894bbef737113d31fcb761c79e18d96f658e
DaniellaJonas <daniella.jonas11@gmail.com>
2024-06-05 13:45:16 +0300

    finishing mediterranean

--- Mediterranean/Salty/saltyM.js
+++ Mediterranean/Salty/saltyM.js
@@ -5,3 +5,8 @@ function Tzatziki(){
 function SEM(){
     window.location.href = "./Spanakopita Egg Muffins/sem.html"
 }
+Moussaka
+
+function Moussaka(){
+    window.location.href = "./Moussaka/moussaka.html"
+}
