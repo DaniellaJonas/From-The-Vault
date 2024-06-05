e6cda9cc5d55de17b661ba18cd24433c22e4c015
DaniellaJonas <daniella.jonas11@gmail.com>
2024-06-05 19:19:29 +0300

    still not working

--- Italian/Salty/Risotto/risotto.js
+++ Italian/Salty/Risotto/risotto.js
@@ -9,7 +9,7 @@ function saved(){
     }
 
     if(!recipes.find(recipe => recipe.name === "Risotto")){
-        recipes.push({name:"Risotto", "link": ".././Italian/Salty/Risotto/Risotto.html", img: "https://i.pinimg.com/564x/ca/71/59/ca71593fc08b0dd55a84d0fa3b98a196.jpg"});
+        recipes.push({name:"Risotto", "link": ".././Italian/Salty/Risotto/risotto.html", img: "https://i.pinimg.com/564x/ca/71/59/ca71593fc08b0dd55a84d0fa3b98a196.jpg"});
         localStorage.setItem("mySavedRecipes",JSON.stringify(recipes));
     }
 }
